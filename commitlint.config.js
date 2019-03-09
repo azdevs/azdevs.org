97e2f32b8a0e24a29b79bb8b2b2c458cdb4c8789
Kevin Lanni <therealklanni@gmail.com>
2019-03-08 23:30:46 -0700

    chore(commitlint): use default types

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,6 @@ module.exports = {
   rules: {
     'header-max-length': [1, 'always', 50],
     'body-max-line-length': [2, 'always', 72],
-    'footer-max-line-length': [2, 'always', 72],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'chore']]
+    'footer-max-line-length': [2, 'always', 72]
   }
 }
