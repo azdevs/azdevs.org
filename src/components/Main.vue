9e2e0b4193ae914f2c8811f49b3c9d26081aeab4
jaysonesmith <github@nes.33mail.com>
2019-03-11 23:38:25 -0700

    refactor: add existing site data in basic components

---
+++ src/components/Main.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p>{az}devs is a community created by and for local developers as a place to connect, communicate, and collaborate. This community aims to be a great place to chat and get help or advice, for those of us who attend every possible meetup, those who can't attend any, and everyone in between.</p>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Main />
+    <NewMember />
+    <ExistingMember />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Main from '@/components/Main.vue'
+import NewMember from '@/components/NewMember.vue'
+import ExistingMember from '@/components/ExistingMember.vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
   components: {
+    Main,
+    NewMember,
+    ExistingMember,
     HelloWorld
   }
 })
