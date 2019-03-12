45f54d6ad4c1671b62848ee1428fcf054b94a326
jaysonesmith <github@nes.33mail.com>
2019-03-11 23:33:06 -0700

    refactor: add existing about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>we are {az}devs</h1>
+    <p>Many local developers don't have time to attend meetups, simply can't go as often as they'd like to, or live too far away from ones they would like to attend. For these reasons, we created this community so more developers can network with each other. This community is less formal than the various Meetup groups, and we don't (yet) have any regular meetings. It's more just a place to gather and chat, collaborate, get help, offer help, advice, network, etc. Join us and meet more devs!</p>
   </div>
 </template>
