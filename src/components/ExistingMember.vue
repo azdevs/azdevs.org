9fed819b2a9cc612468ab367ff4bcad1df59d052
jaysonesmith <github@nes.33mail.com>
2019-03-11 23:38:25 -0700

    refactor: add existing site data in basic components

---
+++ src/components/ExistingMember.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Already a member?</p>
+    <a href="https://slack.azdevs.org" class="btn">Open Slack »</a>
+  </div>
+</template>
---
+++ src/components/NewMember.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Not a member?</p>
+    <a class="btn" href="/signin">Request an invite »</a>
+  </div>
+</template>
